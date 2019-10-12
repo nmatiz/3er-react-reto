f29ce87790bdd2ee7b9e5695b8caea7b54abd2e1
norberto80@gmail.com <namatiz80@gmail.com>
2019-10-12 14:01:13 -0500

    Contador de caracteres V1.6

--- src/App.js
+++ src/App.js
@@ -8,16 +8,19 @@ class App extends Component {
   };
  
 //se crea la funcion primero, que es la que va a realizar el conteo
-  contar(event) {
+  contador(event) {
     this.setState({
+      /**el 'event' es el evento que sucede desde donde se llama, y a este se e asigna la funcion, para este casova capturando la longitud de lo que se tenga en contar, cada vez que cambia*/
       contar: event.target.value.length
     });
   }
-//En el render se pinta la pagina, y en el onChange, se captura lo que va cambiandp
+//En el render se pinta la pagina, y en el onChange, se captura lo que va cambiando
   render() {
     return (
       <div className="container">
-        <textarea rows="3" onChange={this.contar.bind(this)}></textarea>
+        {/**con el onchange en la caja de texto captura los cambios que tenga la caja*/}
+        <textarea rows="3" onChange={this.contador.bind(this)}></textarea>
+        {/**en este div, va mostrando el estado de contar*/}
         <div className="counter">{this.state.contar}</div>
       </div>
     );
